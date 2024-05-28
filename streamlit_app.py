1905c2ba982a4e46442aa32b7b7145f7657b0e54
fsmosca <fsmosca@users.noreply.github.com>
2024-05-28 12:38:12 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+st.set_page_config(layout='centered')
+
+
+def Home():
+    st.subheader('Home')
+
+
+def main():
+    pg = st.navigation({
+        "Overview": [
+            st.Page(Home, title="Home", default=True, icon=":material/home:")
+        ],
+        "Sports": [
+            st.Page('sports/basketball.py', title="Basketball", icon=":material/sports_basketball:"),
+            st.Page('sports/cricket.py', title="Cricket", icon=":material/sports_cricket:"),
+            st.Page('sports/soccer.py', title="Soccer", icon=":material/sports_soccer:")
+        ]
+    })
+
+    try:
+        pg.run()
+    except Exception as e:
+        st.error(f"Something went wrong: {str(e)}", icon=":material/error:")
+
+
+if __name__ == '__main__':
+    main()
